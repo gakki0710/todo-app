ae60c543d5a72959b9163c4827f1c6961c383fbe
gakki <t_higashigaki@yahoo.co.jp>
2026-09-21 11:11:43 +0900

    初回コミット

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,42 @@
+package com.example.todo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
+public class TodoController {
+
+    /**
+     * TODO一覧を取得し、一覧画面を表示する。
+     *
+     * @param model ビューへ渡すデータを保持するModel
+     * @return TODO一覧画面のビュー名
+     */
+    @GetMapping
+    public String index(Model model) {
+        return "index";
+    }
+
+    /**
+     * TODO登録画面を表示する。
+     *
+     * @return TODO登録画面のビュー名
+     */
+    @GetMapping("/add")
+    public String add() {
+        return "add";
+    }
+
+    /**
+     * TODO更新画面を表示する。
+     *
+     * @return TODO更新画面のビュー名
+     */
+    @GetMapping("/edit")
+    public String edit() {
+        return "edit";
+    }
+}
